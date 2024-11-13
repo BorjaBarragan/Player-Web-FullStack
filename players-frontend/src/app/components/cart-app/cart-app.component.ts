f53b190a8fa51d7d1254f2a32b4d62f621e27c8d
BorjaBarragan <borja.barragan.92@gmail.com>
2024-11-13 17:47:26 +0100

    Añadimos sessionStorage para guardar datos del carroCompra al actualizar

--- players-frontend/src/app/components/cart-app/cart-app.component.ts
+++ players-frontend/src/app/components/cart-app/cart-app.component.ts
@@ -25,13 +25,21 @@ export class CartAppComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    //JSON.parse convierte una cadena de texto en formato JSON de nuevo en un objeto o array de JavaScript.
+    //La expresión || [] se utiliza como una medida de seguridad para asegurar que this.items tenga siempre
+    // un valor válido, incluso si no hay nada guardado en sessionStorage
+    //añadimos !, para eliminar el posible error y asegurar al codigo de que siempre contendrá un dato.
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
+    //colocamos el calculateTotal al final, ya que sino el sessionStorage pierde la cantidad y queda en 0 al actualizar.
     this.calculateTotal();
   }
 
   onAddCart(product: Product): void {
+    //find, buscamos si ya existe el producto que hay en el carrito, con el que queremos agregar (el que entra por parametro.)
     const hasItem = this.items.find(item => item.product.id === product.id);
+    //si ese producto que queremos agregar (product.id), esta en el carrito (item.product.id), lo almacena en hasItem
     if (hasItem) {
-      //map devuelve una nueva instancia de los items modificado.
+      //map devuelve una nueva instancia de los items modificado su cantidad +1.
       this.items = this.items.map(item => {
         if (item.product.id === product.id) {
           return {
@@ -39,20 +47,34 @@ export class CartAppComponent implements OnInit {
             quantity: item.quantity + 1
           }
         }
+        //sino coincide, devuelve el item sinc ambios
         return item;
       })
+      //si hasItem es undefined, significa que el producto no está en el carrito. En este caso, lo agregamos como un nuevo elemento.
     } else {
       this.items = [...this.items, { product: { ...product }, quantity: 1 }];
     }
     this.calculateTotal();
+    this.saveSession()
   }
 
   onDeleteCart(id: number): void {
+    //filter crea un nuevo array sin el producto cuyo id coincide.
     this.items = this.items.filter(item => item.product.id !== id);
-    this.calculateTotal()
+    //recordar item es un elemento del array items. Podría llamarse cualquier otra cosa.
+    this.calculateTotal();
+    this.saveSession();
   }
 
   calculateTotal(): void {
+    //el metodo reduce se utiliza para recorrer un array y "reducirlo" a un solo valor
+    //array.reduce((accumulator, currentValue) => { /* lógica de acumulación */ }, initialValue);
     this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0)
+  }
+
+  saveSession() : void{
+    //JSON.stringify convierte un objeto o array de JavaScript en una cadena de texto en formato JSON.
+    //setItem(clave,valor)
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
   }
 }
